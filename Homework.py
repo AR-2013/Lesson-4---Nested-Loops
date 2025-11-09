7aabd05fe722be3b7a15029a55cd71e5e063f202
AR-2013 <anairarandeva@icloud.com>
2025-11-09 10:48:30 +0400

    Homework

--- Homework.py
+++ Homework.py
@@ -5,6 +5,6 @@ binary = ""
 while decimal > 0:
     remainder = decimal % 2          
     binary = str(remainder) + binary 
-    decimal = decimal // 2           
-
+    decimal = decimal // 2          
+     
 print("Binary number is:", binary)
